83ba3814e11d00a8d7ebdb6ff4b73a287f5dfba2
hmathir <hmathir@gmail.com>
2022-09-02 00:45:45 +0600

    Fetching Meal Data Using Search By Meal Name API

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//Fetch Meal Data From API
+const loadMeal = async (searchValue) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`
+    const response = await fetch(url);
+    const meals = await response.json();
+    displayMeals(meals.meals);
+}
+
+loadMeal('');
+
+
+//Write a function for display meal data which got from 'loadMeal' function.
+const displayMeals = (meals) => {
+    const mealCardParent = document.getElementById('parent-meal-view');
+    meals.forEach(meal => {
+        const mealCard = document.createElement('div');
+        mealCard.classList.add('col');
+        mealCard.innerHTML = `
+        <div class="card h-100">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0,150)}</p>
+        </div>
+    </div>`;
+    mealCardParent.appendChild(mealCard);
+    })
+}
